f69a765f60ecd3c402e7cce0e9349b7f3ebb954b
Claude Lee <calee2005@outlook.com>
2018-05-25 13:15:21 +0800

    minor changes

--- test/02_sgf_parser.test.js
+++ test/02_sgf_parser.test.js
@@ -1,9 +1,11 @@
 const tap = require('tap');
 const sgf = require('../index')
 
-var simple = sgf.parse('(;FF[4]GN[Simple SGF\\]];B[aa];W[bb])');
+var simple = sgf.parse('(;FF[4]GN[Simple SGF\\]];B[aa];W[bb]C[Comment]N[Node Name])');
 
 tap.equal(simple.length, 1);
 tap.equal(simple[0].info['GN'], 'Simple SGF]');
 tap.equal(simple[0].rootNode.children[0].children[0].color, 'W');
+tap.equal(simple[0].rootNode.children[0].children[0].comment, 'Comment');
+tap.equal(simple[0].rootNode.children[0].children[0].name, 'Node Name');
 tap.equal(simple[0].rootNode.children[0].children[0].position[1], 1);
